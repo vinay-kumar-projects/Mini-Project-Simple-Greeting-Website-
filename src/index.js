15819ad899017e0171b9ce942c48f2ea714693fb
vinay-kumar-projects <vinayprojects@gmail.com>
2024-02-02 15:20:38 +0530

    Simple Greeting Website Created Using ReactJS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let curDate = new Date(2024, 2, 2, 15);
+curDate = curDate.getHours();
+let greeting = '';
+let cssStyle = {};
+
+if(curDate >= 1 && curDate <12){
+    greeting = 'Good Mornig';
+    cssStyle.color = 'Green';
+    const heading = {
+        color: "#fa9191",
+        textTransform: "capitalize",
+        textAlign: "center",
+        fontWeight: "bold",
+        fontFamily: "'Josefin Sans', sans-serif",
+        margin: "50px"
+    }
+    } else if(curDate >=12 && curDate <19){
+        greeting = 'Good AfterNoon';
+        cssStyle.color = 'Orange';
+    } else {
+        greeting = 'Good Night';
+        cssStyle.color = 'Black';
+    }
+
+ReactDOM.render(
+    <>
+    <h1 className='heading'>Hello sir, <span style={cssStyle}> { greeting } </span></h1>
+    </>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
